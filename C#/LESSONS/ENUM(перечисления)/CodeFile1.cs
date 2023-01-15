1b4bf9937b2eae2e11962375dd80e0462dd1f946
Andrey Sholohov <catclawtooth@gmail.com>
2023-01-15 21:38:12 +0300

    Добавлен урок ENUM

---
+++ C#/LESSONS/ENUM(перечисления)/CodeFile1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENUM_перечисления_
+{
+    internal class Program2 // создание собственного ENUM
+    {
+        enum DayOfWeek :byte // указываем тип данных в котором собираемся хранить
+        {
+            Mondey = 1,
+            Tuesday = 2,
+            Wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6,
+            Sunday = 7
+        }
+        static void Main(string[] args)
+        {
+            DayOfWeek dayOfWeek = DayOfWeek.Mondey;
+
+            Console.WriteLine(Enum.GetUnderlyingType(typeof(DayOfWeek))); // Выводим в консоль тип данных использующийся в нашем enum
+
+            Console.WriteLine(dayOfWeek);
+
+        }
+    }
+}
---
+++ C#/LESSONS/ENUM(перечисления)/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENUM_перечисления_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выбор программы:");
+            while (true)
+            {
+                ConsoleKey key = Console.ReadKey().Key;
+
+                int keyCode = (int)key;
+
+                Console.WriteLine($"\tEnum{key}\tKeyCode{keyCode}");
+
+                if (key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine("Вы нажали enter");
+                }
+            }
+        }
+    }
+}
